02da62c4d295bff225e6acf982b0834cea27bf7a
Christoph M. Becker <cmbecker69@gmx.de>
2025-04-09 23:59:04 +0200

    Add minimalist shim for XH\Mail
    This may be useful for plugins which already use XH\Mail for sending
    emails, but for whatever reason want to switch using PHPMailer under
    the hood, by just switching the namespace.  This should work for some
    typical cases, but is not supposed to ever provide full compatibility
    with XH\Mail.

---
+++ classes/Mail.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace PHPMailer;
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+/**
+ * @author    Peter Harteg <peter@harteg.dk>
+ * @author    The CMSimple_XH developers <devs@cmsimple-xh.org>
+ * @author    Christoph M. Becker
+ * @copyright 1999-2009 Peter Harteg
+ * @copyright 2009-2019 The CMSimple_XH developers <http://cmsimple-xh.org/?The_Team>
+ * @copyright 2025 Christoph M. Becker
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @see       http://cmsimple-xh.org/
+ */
+class Mail
+{
+    /** @var PHPMailer */
+    private $mailer;
+
+    public function __construct()
+    {
+        $this->mailer = new PHPMailer();
+        $this->mailer->CharSet = PHPMailer::CHARSET_UTF8;
+    }
+
+    public function isValidAddress(string $address): bool
+    {
+        return $this->mailer->validateAddress($address);
+    }
+
+    public function setTo(string $to): void
+    {
+        // TODO: split $to in $address and $name?
+        // TODO: reset if called multiple times?
+        $this->mailer->addAddress($to);
+    }
+
+    public function getSubject(): string
+    {
+        return $this->mailer->encodeHeader($this->mailer->secureHeader($this->mailer->Subject));
+    }
+
+    public function setSubject(string $subject): void
+    {
+        $this->mailer->Subject = $subject;
+    }
+
+    public function setMessage(string $message): void
+    {
+        // TODO: force base64 encoding?
+        $this->mailer->Body = $message;
+    }
+
+    public function addHeader(string $name, string $value): void
+    {
+        // TODO: allow more headers?
+        assert(in_array(strtolower($name), ["from", "cc", "bcc", "reply-to"]));
+        // TODO: split $value in $address and $name?
+        switch (strtolower($name)) {
+            case "from":
+                $this->mailer->setFrom($value);
+                break;
+            case "cc":
+                $this->mailer->addCC($value);
+                break;
+            case "bcc":
+                $this->mailer->addBCC($value);
+                break;
+            case "reply-to":
+                $this->mailer->addReplyTo($value);
+                break;
+        }
+    }
+
+    public function send(): bool
+    {
+        return $this->mailer->send();
+    }
+}
